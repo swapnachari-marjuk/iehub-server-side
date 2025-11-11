6402b41e22ee1a13efd91a86c14cbe79711f747e
swapnachari-marjuk <marjukmujaddedi@gmail.com>
2025-11-11 13:42:47 +0600

    implemented delete functionalities in db

--- index.js
+++ index.js
@@ -106,6 +106,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/imports/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await importsColl.deleteOne(query);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
